ea363bb531cb586ed1af57082f504bf5d0ca2161
Amul Sinfal <amul.sinfal@gmail.com>
2025-07-18 00:36:35 +0530

    Committed files.

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -15,26 +15,27 @@ import utilities.ConfigReader;
 public class DriverFactory {
 	private static WebDriver driver;
 	private static final Logger log = LogManager.getLogger(DriverFactory.class);
-	
+
 	public static WebDriver initializeDriver(String browser) {
-		if(browser.equalsIgnoreCase("Chrome")) {
+		if (browser.equalsIgnoreCase("Chrome")) {
 			ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.addArguments("--incognito");
 			driver = new ChromeDriver(chromeOptions);
-		} else if(browser.equalsIgnoreCase("Firefox")) {
+		} else if (browser.equalsIgnoreCase("Firefox")) {
 			driver = new FirefoxDriver();
-		} else if(browser.equalsIgnoreCase("Edge")) {
+		} else if (browser.equalsIgnoreCase("Edge")) {
 			driver = new EdgeDriver();
 		}
 		log.info(browser + " browser launched.");
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("QA", "implicitWait"))));
-		log.info("Implicit wait set to " + ConfigReader.getValue("QA", "implicitWait") +" seconds." );
+		driver.manage().timeouts()
+				.implicitlyWait(Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("QA", "implicitWait"))));
+		log.info("Implicit wait set to " + ConfigReader.getValue("QA", "implicitWait") + " seconds.");
 		driver.manage().window().maximize();
-		log.info("Webdriver initialised successfully." );
+		log.info("Webdriver initialised successfully.");
 		return driver;
 	}
-	
+
 	public static WebDriver getDriver() {
 		return driver;
 	}
